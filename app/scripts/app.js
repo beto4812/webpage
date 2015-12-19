5c2c90b34d8fb96caec40a5d363636b6858c6b04
Alberto Vazquez <s1568644@ed.ac.uk>
2015-12-19 15:16:24 +0000

    My webpage

---
+++ app/scripts/app.js
@@ -0,0 +1,12 @@
+'use strict';
+
+/**
+ * @ngdoc overview
+ * @name webPageApp
+ * @description
+ * # webPageApp
+ *
+ * Main module of the application.
+ */
+angular
+  .module('webPageApp', ['smoothScroll']);
---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name webPageApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the webPageApp
+ */
+angular.module('webPageApp')
+  .controller('MainCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
