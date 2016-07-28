2b177999e6f3bc249df4a3de5ac555a3bde5d0fb
Alberto Vazquez <beto4812@hotmail.com>
2016-07-28 18:37:32 +0100

    Changes

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -8,5 +8,22 @@
  *
  * Main module of the application.
  */
-angular
+var myApp = angular
   .module('webPageApp', ['smoothScroll']);
+
+  myApp.directive('carousel', function($timeout) {
+   return {
+      restrict: 'E',
+      scope: {
+        carouselId: '@',
+        links: '='
+      },
+      templateUrl: 'views/carousel.html',
+      link: function(scope, element) {
+        $timeout(function() {
+          $('.carousel-indicators li',element).first().addClass('active');
+          $('.carousel-inner .item',element).first().addClass('active');
+        });
+      }
+   }
+});
--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -8,10 +8,21 @@
  * Controller of the webPageApp
  */
 angular.module('webPageApp')
-  .controller('MainCtrl', function () {
+  .controller('MainCtrl', function ($scope) {
     this.awesomeThings = [
       'HTML5 Boilerplate',
       'AngularJS',
       'Karma'
     ];
+    $scope.robocupCarousel =[
+      { src:"images/robocup.jpg", alt:"", caption:"Robocup 2014, Brazil"},
+      { src:"images/robocup2.jpg", alt:"", caption:"Robocup 2013, Netherlands"},
+      { src:"images/robocup3.jpg", alt:"", caption:"Robocup 2013, Netherlands"}
+   ];
+
+   $scope.airQueueCarousel =[
+     { src:"images/airqueue.png", alt:"", caption:""},
+     { src:"images/airqueue2.png", alt:"", caption:""},
+     { src:"images/airqueue3.png", alt:"", caption:""}
+  ];
   });
