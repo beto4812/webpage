d933287041a11465fa8ee530dd017dd4526f9af4
Alberto Vazquez <beto4812@hotmail.com>
2016-11-29 22:43:11 -0600

    Updates

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -26,3 +26,24 @@ angular.module('webPageApp')
      { src:"images/airqueue3.png", alt:"", caption:""}
   ];
   });
+
+  $('.more-skill').bind('inview', function(event, visible, visiblePartX, visiblePartY) {
+    if (visible) {
+        $('.chart').easyPieChart({
+            //your configuration goes here
+            easing: 'easeOut',
+            delay: 3000,
+            barColor:'#68c3a3',
+            trackColor:'rgba(255,255,255,0.2)',
+            scaleColor: false,
+            lineWidth: 8,
+            size: 140,
+            animate: 2000,
+            onStep: function(from, to, percent) {
+                this.el.children[0].innerHTML = Math.round(percent);
+            }
+
+        });
+        $(this).unbind('inview');
+    }
+});
